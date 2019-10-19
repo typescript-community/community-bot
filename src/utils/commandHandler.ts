a9a827ac1753908edf97beb112bbb45fa03012d2
Robert Westbury <haxified1@gmail.com>
2019-10-19 12:27:07 +0100

    Removed command not found message

--- src/utils/commandHandler.ts
+++ src/utils/commandHandler.ts
@@ -24,18 +24,10 @@ interface CommandOptions {
 // Command Handler Class
 export class CommandHandler {
     private commands: Command[] = [];
-    private commandNotFoundFn: (message: Message) => Promise<void>;
 
     constructor(private bot: Client, private option: CommandHandlerOptions) {
         // Logger
         this.option.logger = option.logger || console.log;
-
-        // Not found command function
-        if (!option.commandNotFoundFnOrMessage || typeof option.commandNotFoundFnOrMessage == 'string') {
-            this.commandNotFoundFn = async (message: Message): Promise<void> => {
-                await message.channel.send(option.commandNotFoundFnOrMessage || 'Command Not Found');
-            };
-        }
 
         // Add Help Command
         this.addHelpCommand();
@@ -98,9 +90,6 @@ export class CommandHandler {
 
             await Promise.all(commandsRan);
         }
-
-        // If no commands found run the no command found function
-        else await this.commandNotFoundFn(message);
     }
 
     /**
