4aa4a930d33cf999094a9d11ac9833f24ccb35a6
Robert Westbury <haxified1@gmail.com>
2019-10-19 15:48:57 +0100

    hotfix no 3

--- src/commands/removeRep.ts
+++ src/commands/removeRep.ts
@@ -16,7 +16,7 @@ export const command = new Command({
         }
 
         let member: GuildMember | undefined = message.mentions.members!.first()!;
-        member = !member ? await resolvMember(message) : member;
+        member = !member ? await resolveMember(message) : member;
 
         const amount = parseInt(message.content.split(' ')[2]);
 
