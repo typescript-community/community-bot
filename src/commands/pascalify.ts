949156ef41c09f3fd606b9bf52113b55ca49c2d5
Nekoniri <1681034+Nekoniri@users.noreply.github.com>
2019-10-24 14:57:35 +0200

    Only Pascalify the message after the command instead of the whole thing

--- src/commands/pascalify.ts
+++ src/commands/pascalify.ts
@@ -1,11 +1,18 @@
 import { Message } from 'discord.js';
+import PascalCase from 'pascal-case';
 
 import { Command } from '../utils/commandHandler';
 
-import PascalCase from 'pascal-case';
-
 export const command = new Command({
     description: 'Converts user message to PascalCase',
-    aliases: ['PascalCase', 'Pascalify', 'pascalify', 'Robinify'],
-    command: async (message: Message): Promise<Message> => message.channel.send(PascalCase(message.content)),
+    aliases: ['PascalCase', 'Pascalify', 'pascalify', 'Robinify', 'robinify'],
+    command: async (message: Message): Promise<Message> =>
+        message.channel.send(
+            PascalCase(
+                message.content
+                    .split(' ')
+                    .slice(1)
+                    .join(' '),
+            ),
+        ),
 });
