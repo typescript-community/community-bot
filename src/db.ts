1ffda918d10d85cf4c4cf2e4c14baefa85433f77
Ron B <me@ronthecookie.me>
2020-08-01 16:00:54 +0300

    chore: add env vars for db details

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,15 @@
 import { Connection, createConnection } from "typeorm";
 import { Reminder } from "./modules/reminders";
+import { db as dbEnv } from "./env";
 
 let db: Connection | undefined;
 export async function getDB() {
 	if (db) return db;
 	db = await createConnection({
 		type: "postgres",
-		host: "localhost",
-		port: 5432,
-		username: "ron",
+		host: dbEnv.host,
+		port: dbEnv.port,
+		username: dbEnv.user,
 		database: "tsc-bot",
 		synchronize: true,
 		logging: true,
--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv-safe";
 dotenv.config();
 
-export const token = process.env.TOKEN;
+export const token = process.env.TOKEN!;
 export const botAdmins = process.env.BOT_ADMINS!.split(",");
 export const autorole = process.env.AUTOROLE!.split(",").map(x => {
 	const [msgID, roleID, emoji, autoRemove] = x.split(":");
@@ -12,5 +12,11 @@ export const autorole = process.env.AUTOROLE!.split(",").map(x => {
 		autoRemove: autoRemove == "true",
 	};
 });
+
+export const db = {
+	host: process.env.DB_HOST!,
+	port: parseInt(process.env.DB_PORT!, 10),
+	user: process.env.DB_USER!,
+};
 
 export const TS_BLUE = "#007ACC";
