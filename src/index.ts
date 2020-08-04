96a95cbbda5d4c9a8aef86dc9f85ecc2792846e4
Robert Westbury <haxified1@gmail.com>
2020-08-04 18:16:51 +0100

    fix: load modules from dist in prod

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const client = new CookiecordClient(
 );
 const prod = process.env.NODE_ENV == 'production';
 
-client.loadModulesFromFolder('src/modules');
+client.loadModulesFromFolder(prod ? 'dist/modules' : 'src/modules');
 if (!prod) client.reloadModulesFromFolder('src/modules');
 
 getDB(); // prepare the db for later
