f80e90d922506785a5591342bed2fbbb25ba9afa
Robert Westbury <haxified1@gmail.com>
2020-08-04 18:30:27 +0100

    fix: misspelling of database url in env file

--- src/env.ts
+++ src/env.ts
@@ -16,7 +16,7 @@ export const autorole = process.env.AUTOROLE!.split(',').map(x => {
     };
 });
 
-export const dbUrl = process.env.DATABASE_UR!;
+export const dbUrl = process.env.DATABASE_URL!;
 
 export const categories = {
     ask: process.env.ASK_CATEGORY!,
