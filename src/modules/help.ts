cfe5cd808212a010366eb2f2ab9312093446d612
Ron B <me@ronthecookie.me>
2020-08-27 15:25:37 +0300

    rename message->msg for consistency

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -7,16 +7,13 @@ export default class HelpModule extends Module {
 	}
 
 	@command({ aliases: ['help', 'commands', 'h'] })
-	async help(message: Message, args: string) {
+	async help(msg: Message, args: string) {
 		if (!args) {
 			let embed = new MessageEmbed()
-				.setAuthor(
-					message.guild.name,
-					message.guild.iconURL({ dynamic: true }),
-				)
+				.setAuthor(msg.guild.name, msg.guild.iconURL({ dynamic: true }))
 				.setTitle('Help Command!')
 				.setDescription(
-					`Hello ${message.author.username}! Here is a list of all commands in me! To get detailed description on any specific command, do \`help <command>\``,
+					`Hello ${msg.author.username}! Here is a list of all commands in me! To get detailed description on any specific command, do \`help <command>\``,
 				)
 				.addField(
 					`**Misc Commands:**`,
@@ -36,46 +33,46 @@ export default class HelpModule extends Module {
 				)
 				.setTimestamp();
 
-			return message.channel.send(embed);
+			return msg.channel.send(embed);
 		} else {
 			switch (args[0]) {
 				case 'ping':
-					message.channel.send(
+					msg.channel.send(
 						`Aliases: \`ping\`\nDescription: *View the latency of the bot*\nUsage: \`ping\``,
 					);
 					break;
 				case 'ask':
-					message.channel.send(
+					msg.channel.send(
 						`Aliases: \`ask\`\nDescription: *Bot sends a dontasktoask website link*\nUsage: \`ask\``,
 					);
 					break;
 				case 'done':
-					message.channel.send(
+					msg.channel.send(
 						`Aliases: \`done\`, \`close\`, \`resolve\`\nDescription: *Close a opened help channel*\nUsage: \`close\``,
 					);
 					break;
 				case 'rep':
-					message.channel.send(
+					msg.channel.send(
 						`Aliases: \`rep\`\nDescription: *Give a user some reputation*\nUsage: \`rep <user>\``,
 					);
 					break;
 				case 'history':
-					message.channel.send(
+					msg.channel.send(
 						`Aliases: \`getrep\`, \`history\`\nDescription: *Get the reputation history of a user*\nUsage: \`history <user>\``,
 					);
 					break;
 				case 'leaderboard':
-					message.channel.send(
+					msg.channel.send(
 						`Aliases: \`leaderboard\`, \`lb\`\nDescription: *View the reputation leaderboard!*\nUsage: \`leaderboard\``,
 					);
 					break;
 				case 'help':
-					message.channel.send(
+					msg.channel.send(
 						`Aliases: \`help\`, \`commands\`, \`h\`\nDescription: *Get the list of commands in me!*\nUsage: \`help [command (optional)]\``,
 					);
 					break;
 				default:
-					message.channel.send(`No command found!`);
+					msg.channel.send(`No command found!`);
 			}
 		}
 	}
