a524726645ed64ecce6abca3f0279d7e363dc561
Ron B <me@ronthecookie.me>
2020-08-27 15:40:39 +0300

    utilize cookiecord's command description field

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -16,16 +16,16 @@ export default class HelpModule extends Module {
 		aliases: ['help', 'commands', 'h'],
 		inhibitors: [CommonInhibitors.guildsOnly],
 	})
-	async help(msg: Message, @optional cmd?: string) {
+	async help(msg: Message, @optional cmdTrigger?: string) {
 		if (!msg.guild) return;
 
-		if (!cmd) {
+		if (!cmdTrigger) {
 			let embed = new MessageEmbed()
 				.setAuthor(
 					msg.guild.name,
 					msg.guild.iconURL({ dynamic: true }) || undefined,
 				)
-				.setTitle('Help Command!')
+				.setTitle('Bot Usage')
 				.setDescription(
 					`Hello ${msg.author.username}! Here is a list of all commands in me! To get detailed description on any specific command, do \`help <command>\``,
 				)
@@ -47,47 +47,53 @@ export default class HelpModule extends Module {
 				)
 				.setTimestamp();
 
-			return msg.channel.send(embed);
+			return await msg.channel.send(embed);
 		} else {
-			switch (cmd[0]) {
-				case 'ping':
-					msg.channel.send(
-						`Aliases: \`ping\`\nDescription: *View the latency of the bot*\nUsage: \`ping\``,
-					);
-					break;
-				case 'ask':
-					msg.channel.send(
-						`Aliases: \`ask\`\nDescription: *Bot sends a dontasktoask website link*\nUsage: \`ask\``,
-					);
-					break;
-				case 'done':
-					msg.channel.send(
-						`Aliases: \`done\`, \`close\`, \`resolve\`\nDescription: *Close a opened help channel*\nUsage: \`close\``,
-					);
-					break;
-				case 'rep':
-					msg.channel.send(
-						`Aliases: \`rep\`\nDescription: *Give a user some reputation*\nUsage: \`rep <user>\``,
-					);
-					break;
-				case 'history':
-					msg.channel.send(
-						`Aliases: \`getrep\`, \`history\`\nDescription: *Get the reputation history of a user*\nUsage: \`history <user>\``,
-					);
-					break;
-				case 'leaderboard':
-					msg.channel.send(
-						`Aliases: \`leaderboard\`, \`lb\`\nDescription: *View the reputation leaderboard!*\nUsage: \`leaderboard\``,
-					);
-					break;
-				case 'help':
-					msg.channel.send(
-						`Aliases: \`help\`, \`commands\`, \`h\`\nDescription: *Get the list of commands in me!*\nUsage: \`help [command (optional)]\``,
-					);
-					break;
-				default:
-					msg.channel.send(`No command found!`);
-			}
+			const cmd = this.client.commandManager.getByTrigger(cmdTrigger);
+			await msg.channel.send(
+				`Usage: \`${cmd?.triggers.join('|')} ${cmd?.args.map(arg =>
+					arg.optional ? `[${arg.type.name}]` : `<${arg.type.name}>`,
+				)}\`${
+					cmd?.description
+						? `\nDescription: *${cmd?.description}*`
+						: ''
+				}`,
+			);
+			// case 'ping':
+			// 	msg.channel.send(
+			// 		`Aliases: \`ping\`\nDescription: *View the latency of the bot*\nUsage: \`ping\``,
+			// 	);
+			// 	break;
+			// case 'ask':
+			// 	msg.channel.send(
+			// 		`Aliases: \`ask\`\nDescription: *Bot sends a dontasktoask website link*\nUsage: \`ask\``,
+			// 	);
+			// 	break;
+			// case 'done':
+			// 	msg.channel.send(
+			// 		`Aliases: \`done\`, \`close\`, \`resolve\`\nDescription: *Close a opened help channel*\nUsage: \`close\``,
+			// 	);
+			// 	break;
+			// case 'rep':
+			// 	msg.channel.send(
+			// 		`Aliases: \`rep\`\nDescription: *Give a user some reputation*\nUsage: \`rep <user>\``,
+			// 	);
+			// 	break;
+			// case 'history':
+			// 	msg.channel.send(
+			// 		`Aliases: \`getrep\`, \`history\`\nDescription: *Get the reputation history of a user*\nUsage: \`history <user>\``,
+			// 	);
+			// 	break;
+			// case 'leaderboard':
+			// 	msg.channel.send(
+			// 		`Aliases: \`leaderboard\`, \`lb\`\nDescription: *View the reputation leaderboard!*\nUsage: \`leaderboard\``,
+			// 	);
+			// 	break;
+			// case 'help':
+			// 	msg.channel.send(
+			// 		`Aliases: \`help\`, \`commands\`, \`h\`\nDescription: *Get the list of commands in me!*\nUsage: \`help [command (optional)]\``,
+			// 	);
+			// 	break;
 		}
 	}
 }
