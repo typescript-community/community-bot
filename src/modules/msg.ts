f1690327fda3d68e43ff767daf5fc7c41ddf6d77
NimperX <nimnaperera98@gmail.com>
2020-10-04 19:58:27 +0530

    Minimize code lines

--- src/modules/msg.ts
+++ src/modules/msg.ts
@@ -1,35 +1,23 @@
-const okHand: string = ':ok_hand:';
-const pingPong: string = 'pong. :ping_pong:';
-const dontAskToAsk: string = 'https://dontasktoask.com/';
-const reactfcMsg: string =
+export const okHand: string = ':ok_hand:';
+export const pingPong: string = 'pong. :ping_pong:';
+export const dontAskToAsk: string = 'https://dontasktoask.com/';
+export const reactfcMsg: string =
 	'https://github.com/facebook/create-react-app/pull/8177#issue-353062710';
-const onlyRunInHelp: string =
+export const onlyRunInHelp: string =
 	':warning: you can only run this in ongoing help channels.';
-const beAskerToCloseChannel: string =
+export const beAskerToCloseChannel: string =
 	':warning: you have to be the asker to close the channel.';
-const couldntFindCodeblock: string = ":warning: couldn't find a codeblock!";
-const canRemoveFullLink: string =
+export const couldntFindCodeblock: string =
+	":warning: couldn't find a codeblock!";
+export const canRemoveFullLink: string =
 	"Here's a shortened URL of your playground link! You can remove the full link from your message.";
-const syntaxWarning: string = ':warning: syntax: !remind <duration> [message]';
-const invalidDuration: string = ':warning: invalid duration!';
-const cannotSendRepToYou: string = ':x: you cannot send rep to yourself';
-const noRepRemain: string = ':warning: no rep remaining! come back later.';
-const needValidSymbol: string =
+export const syntaxWarning: string =
+	':warning: syntax: !remind <duration> [message]';
+export const invalidDuration: string = ':warning: invalid duration!';
+export const cannotSendRepToYou: string = ':x: you cannot send rep to yourself';
+export const noRepRemain: string =
+	':warning: no rep remaining! come back later.';
+export const needValidSymbol: string =
 	'You need to give me a valid symbol name to look for!';
-const noTypescriptCode: string =
+export const noTypescriptCode: string =
 	':warning: could not find any TypeScript codeblocks in the past 10 messages';
-
-export { okHand };
-export { pingPong };
-export { dontAskToAsk };
-export { reactfcMsg };
-export { onlyRunInHelp };
-export { beAskerToCloseChannel };
-export { couldntFindCodeblock };
-export { canRemoveFullLink };
-export { syntaxWarning };
-export { invalidDuration };
-export { cannotSendRepToYou };
-export { noRepRemain };
-export { needValidSymbol };
-export { noTypescriptCode };
