dec2d9b3a3b1672e3b2251cf4817b70da5695d12
tjjfvi <tjjfvi@tjjfvi.com>
2021-10-09 10:43:23 -0700

    delete and kick spam posted in multiple channels

--- src/modules/mod.ts
+++ src/modules/mod.ts
@@ -1,11 +1,23 @@
 import { default as CookiecordClient, Module, listener } from 'cookiecord';
-import { Message } from 'discord.js';
+import { Message, Snowflake, User } from 'discord.js';
 import { rulesChannelId } from '../env';
 
 // Most job posts are in this format:
 // > [FOR HIRE][REMOTE][SOMETHING ELSE]
 // > Hi, I'm ponyman6000. Hire me!
 const jobPostRegex = /^(?:\[[A-Z ]+\]){2,}\n/i;
+
+const SPAM_CHANNEL_THRESHOLD = 3;
+const SPAM_MAX_TIME = 5000;
+
+interface RecentMessageInfo {
+	author: User;
+	firstPost: Date;
+	channels: Set<Snowflake>;
+	messages: Message[];
+}
+const recentMessages = new Map<string, RecentMessageInfo>();
+const CLEANUP_RECENT_MESSAGES_MAP_INTERVAL = 10000;
 
 export class ModModule extends Module {
 	constructor(client: CookiecordClient) {
@@ -20,4 +32,43 @@ export class ModModule extends Module {
 			`${msg.author} We don't do job posts here; see <#${rulesChannelId}>`,
 		);
 	}
+
+	@listener({ event: 'message' })
+	async onRepeatedMessage(msg: Message) {
+		if (!msg.guild) return;
+		const messageIdentifier = msg.content.trim().toLowerCase();
+		if (!messageIdentifier) return;
+		let recentMessageInfo = recentMessages.get(messageIdentifier);
+		if (
+			recentMessageInfo &&
+			recentMessageInfo.author.id === msg.author.id &&
+			+recentMessageInfo.firstPost + SPAM_MAX_TIME > Date.now()
+		) {
+			recentMessageInfo.channels.add(msg.channel.id);
+			recentMessageInfo.messages.push(msg);
+			if (recentMessageInfo.channels.size >= SPAM_CHANNEL_THRESHOLD) {
+				await Promise.all([
+					msg.guild.members
+						.fetch(msg.author)
+						.then(member => void member.kick('Spam')),
+					...recentMessageInfo.messages.map(msg => void msg.delete()),
+				]);
+			}
+		} else {
+			recentMessages.set(messageIdentifier, {
+				author: msg.author,
+				firstPost: new Date(),
+				channels: new Set([msg.channel.id]),
+				messages: [msg],
+			});
+		}
+	}
 }
+
+setInterval(() => {
+	for (const [messageText, recentMessageInfo] of recentMessages) {
+		if (+recentMessageInfo.firstPost + SPAM_MAX_TIME < Date.now()) {
+			recentMessages.delete(messageText);
+		}
+	}
+}, CLEANUP_RECENT_MESSAGES_MAP_INTERVAL);
